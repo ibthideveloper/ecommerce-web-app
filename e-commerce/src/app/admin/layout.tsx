f1c39b6afd4942914c0b902a42bf51e987152571
MuhmmedIbthikar <muhmmed.ibthikar@agilysys.com>
2024-07-05 13:32:28 +0530

    ++ AdminDasboard

--- e-commerce/src/app/admin/layout.tsx
+++ e-commerce/src/app/admin/layout.tsx
@@ -1,3 +1,7 @@
+import { Nav, NavLink } from "@/components/Nav";
+
+export const dynamic = "force-dynamic";
+
 export default function AdminLayout({
   children,
 }: Readonly<{
@@ -5,8 +9,13 @@ export default function AdminLayout({
 }>) {
   return (
     <>
-      {/* <Nav></Nav> */}
-      <div className=" container my-6"></div>
+      <Nav>
+        <NavLink href="/admin">Dashboard</NavLink>
+        <NavLink href="/admin/products">Products</NavLink>
+        <NavLink href="/admin/users">Customers</NavLink>
+        <NavLink href="/admin/orders">Sales</NavLink>
+      </Nav>
+      <div className="container my-6"> {children}</div>
     </>
   );
 }
---
+++ e-commerce/src/app/admin/products/page.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { PageHeader } from "../_components/PageHeader";
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+export default function AdminProductsPage() {
+  return (
+    <>
+      <div className="flex justify-between items-center gap-4">
+        <PageHeader>Products</PageHeader>
+        <Button asChild>
+          <Link href="/admin/products/new">Add Products</Link>
+        </Button>
+      </div>
+      <ProductsTable />
+    </>
+  );
+}
+
+function ProductsTable() {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-0">
+            <span className="sr-only">Avaialble for Purchase</span>
+          </TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead>Price</TableHead>
+          <TableHead>Orders</TableHead>
+          <TableHead className="w-0">
+            <span className="sr-only">Actions</span>
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody></TableBody>
+    </Table>
+  );
+}
