e7d53aad2baeadba4803828f160fcdebeb375154
MuhmmedIbthikar <muhmmed.ibthikar@agilysys.com>
2024-07-06 09:13:29 +0530

    ++ products form admin

---
+++ e-commerce/src/app/admin/products/_components/ProductForm.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { useState } from "react";
+
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import { formatCurrency } from "@/lib/formatters";
+import { Button } from "@/components/ui/button";
+import { addProduct } from "../../_actions/products";
+import { useFormState, useFormStatus } from "react-dom";
+
+export function ProductForm() {
+  const [error, formAction] = useFormState(addProduct, {});
+  const [priceInCents, setPriceInCents] = useState<number>();
+
+  return (
+    <form action={formAction} className="space-y-8">
+      <div className="space-y-2">
+        <Label htmlFor="name">Name</Label>
+        <Input type="text" id="name" name="name" />
+        {error.name && <div className="text-destructive">{error.name}</div>}
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="priceInCents">Price In Cents</Label>
+        <Input
+          type="number"
+          id="priceInCents"
+          name="priceInCents"
+          value={priceInCents}
+          onChange={(e) => setPriceInCents(Number(e.target.value) || undefined)}
+        />
+        <div className="text-muted-foreground">
+          {formatCurrency((priceInCents || 0) / 100)}
+        </div>
+        {error.priceInCents && (
+          <div className="text-destructive">{error.priceInCents}</div>
+        )}
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="description">Description</Label>
+        <Textarea id="description" name="description" />
+        {error.description && (
+          <div className="text-destructive">{error.name}</div>
+        )}
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="file">File</Label>
+        <Input type="file" id="file" name="file" />
+        {error.file && <div className="text-destructive">{error.name}</div>}
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="image">Image</Label>
+        <Input type="file" id="image" name="image" />
+        {error.image && <div className="text-destructive">{error.name}</div>}
+      </div>
+      <SubmitButton />
+    </form>
+  );
+}
+
+function SubmitButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button type="submit" disabled={pending}>
+      {pending ? "Saving..." : "Save"}
+    </Button>
+  );
+}
--- e-commerce/src/app/admin/products/page.tsx
+++ e-commerce/src/app/admin/products/page.tsx
@@ -9,6 +9,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import db from "@/db/db";
 
 export default function AdminProductsPage() {
   return (
@@ -24,7 +25,21 @@ export default function AdminProductsPage() {
   );
 }
 
-function ProductsTable() {
+async function ProductsTable() {
+  const products = await db.product.findMany({
+    select: {
+      id: true,
+      name: true,
+      priceInCents: true,
+      isAvailableForPurchase: true,
+      _count: { select: { orders: true } },
+    },
+    orderBy: { name: "asc" },
+  });
+  console.log(products);
+
+  if (products.length === 0) return <p> No Products Found !</p>;
+
   return (
     <Table>
       <TableHeader>
