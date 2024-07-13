ef000ead0108d3f339df173aa581e33a72ab4447
MuhmmedIbthikar <muhmmed.ibthikar@agilysys.com>
2024-07-13 17:51:11 +0530

    ++ Basic auth for admin page

--- e-commerce/src/middleware.ts
+++ e-commerce/src/middleware.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import { isValidPassword } from "./lib/isValidPassword";
 
 export async function middleware(req: NextRequest) {
   if ((await isAuthenticated(req)) === false) {
@@ -18,8 +19,6 @@ async function isAuthenticated(req: NextRequest) {
   const [username, password] = Buffer.from(authHeader.split(" ")[1], "base64")
     .toString()
     .split(":");
-
-  console.log(username, password);
   return (
     username === process.env.ADMIN_USERNAME &&
     (await isValidPassword(
